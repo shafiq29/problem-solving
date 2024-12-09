a466779f108cc3e4fdbddb5de2f1e2ee553ba3d7
shafiq <shafiqrobin9@gmail.com>
2024-12-10 01:12:24 +0600

    Added info

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,10 +1,13 @@
+# @Author : Shafiqul Islam
+# @Date   : 10/12/2024
+
 
 def insertionSort(arr):
 
     for i in range(1,len(arr)):
         
         key =  arr[i]
-        j = i-1
+        j   =  i-1
 
         while j>=0 and key >arr[j]:
             arr[j+1] = arr[j]
@@ -15,6 +18,16 @@ def insertionSort(arr):
 
 
 # will sort the following
-arr = [12,14, 15,3 ,9,2]
+arr = [12,14,15,3,9,2]
 insertionSort(arr)
 print(arr)
+
+# Time Complexity
+
+#   Best case       : O(n) , If the list is already sorted, where n is the number of elements in the list.
+#   Average case    : O(n^2 ) , If the list is randomly ordered
+#   Worst case      : O(n^2 ) , If the list is in reverse order
+
+# Space Complexity of Insertion Sort
+
+#   Auxiliary Space : O(1), Insertion sort requires O(1) additional space, making it a space-efficient sorting algorithm.
